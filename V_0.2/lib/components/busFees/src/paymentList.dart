99992a969b172e6c193315f811b401578b4ddc2f
ArpitSharma2800 <1arpit2000sharma@gmail.com>
2021-05-24 18:24:20 +0530

    header

--- V_0.2/lib/components/busFees/src/paymentList.dart
+++ V_0.2/lib/components/busFees/src/paymentList.dart
@@ -15,58 +15,93 @@ class PaymentList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: payment == "Paid"
-            ? FaIcon(
-                FontAwesomeIcons.checkCircle,
-                color: HexColor(ColorsDesign['green']),
-                size: 50,
-              )
-            : FaIcon(
-                FontAwesomeIcons.timesCircle,
-                color: HexColor(ColorsDesign['red']),
-                size: 50,
-              ),
-        title: Text(
-          "Month : May",
-          style: GoogleFonts.quicksand(
-              textStyle: TextStyle(
-                  // fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: ht(context) * 2.5)),
-        ),
-        subtitle: Text('Fees to be paid ₹500'),
-        trailing: Container(
-          width: MediaQuery.of(context).size.width >= 1100
-              ? wt(context) * 10
-              : MediaQuery.of(context).size.width < 650
-                  ? wt(context) * 20
-                  : wt(context) * 20,
-          height: MediaQuery.of(context).size.width >= 1100
-              ? ht(context) * 5
-              : MediaQuery.of(context).size.width < 650
-                  ? ht(context) * 5
-                  : ht(context) * 4,
-          decoration: BoxDecoration(
-            color: payment == "Paid"
-                ? HexColor(ColorsDesign['blue']).withOpacity(0.3)
-                : HexColor(ColorsDesign['blue']),
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          child: Center(
-            child: Text(
-              "Pay",
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 0,
+        child: ListTile(
+          leading: payment == "Paid"
+              ? FaIcon(
+                  FontAwesomeIcons.checkCircle,
+                  color: HexColor(ColorsDesign['green']),
+                  size: 45,
+                )
+              : FaIcon(
+                  FontAwesomeIcons.timesCircle,
+                  color: HexColor(ColorsDesign['red']),
+                  size: 45,
+                ),
+          title: Row(children: <Widget>[
+            Expanded(
+                child: Text(
+              "May",
+              textAlign: TextAlign.center,
               style: GoogleFonts.quicksand(
                   textStyle: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                // fontSize: wt(context) * 1
-              )),
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: ht(context) * 2.5)),
+            )),
+            Expanded(
+                child: Text(
+              "₹500",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.quicksand(
+                  textStyle: TextStyle(
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: ht(context) * 2.5)),
+            )),
+            Expanded(
+                child: Text(
+              "Etc",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.quicksand(
+                  textStyle: TextStyle(
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: ht(context) * 2.5)),
+            )),
+            Expanded(
+              child: Padding(
+                padding: MediaQuery.of(context).size.width >= 1100
+                    ? EdgeInsets.symmetric(horizontal: 100)
+                    : MediaQuery.of(context).size.width < 650
+                        ? EdgeInsets.symmetric(horizontal: 0)
+                        : EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  width: MediaQuery.of(context).size.width >= 1100
+                      ? wt(context) * 10
+                      : MediaQuery.of(context).size.width < 650
+                          ? wt(context) * 20
+                          : wt(context) * 20,
+                  height: MediaQuery.of(context).size.width >= 1100
+                      ? ht(context) * 5
+                      : MediaQuery.of(context).size.width < 650
+                          ? ht(context) * 5
+                          : ht(context) * 4,
+                  decoration: BoxDecoration(
+                    color: payment == "Paid"
+                        ? HexColor(ColorsDesign['blue']).withOpacity(0.3)
+                        : HexColor(ColorsDesign['blue']),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Pay",
+                      style: GoogleFonts.quicksand(
+                          textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        // fontSize: wt(context) * 1
+                      )),
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
+          ]),
+          isThreeLine: false,
         ),
-        isThreeLine: false,
       ),
     );
   }
